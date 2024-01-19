e832ff72d44e31040c38fdb010051c2842e5c9a1
SkagitIan <67756434+SkagitIan@users.noreply.github.com>
2024-01-19 21:42:20 +0000

    update

--- Dashboard.py
+++ Dashboard.py
@@ -26,7 +26,7 @@ def custom_css():
 
 
 def run():
-    custom_css()
+    
     st.set_page_config(
         page_title="Hello",
         page_icon="👋",
@@ -35,22 +35,10 @@ def run():
     st.write("# Welcome to Streamlit! 👋")
 
     st.sidebar.success("Select a demo above.")
-
+    custom_css()
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+
     """
     )
 
